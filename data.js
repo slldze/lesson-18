6e2fa86dd3434aef5974191744889ab2147611c0
slldze <salome.leladze.111@gmail.com>
2026-05-15 15:48:46 +0400

    second task

--- data.js
+++ data.js
@@ -4,3 +4,8 @@ function isempty(text) {
 console.log(isempty("")); // true
 console.log(isempty(" ")); // false
 console.log(isempty("hello")); // false
+
+function convertdollartogel(priceindollar, gelrate) {
+  return priceindollar * gelrate + "₾";
+}
+console.log(convertdollartogel(100, 2.7));
