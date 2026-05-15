43e63d0a1119da1220341d3c7c2d84f409c23e14
slldze <salome.leladze.111@gmail.com>
2026-05-15 15:53:02 +0400

    third task

--- data.js
+++ data.js
@@ -9,3 +9,21 @@ function convertdollartogel(priceindollar, gelrate) {
   return priceindollar * gelrate + "₾";
 }
 console.log(convertdollartogel(100, 2.7));
+
+function getcurencycountry(curencycode) {
+  if (curencycode) {
+    if (curencycode === "USD") {
+      return "United States";
+    } else if (curencycode === "EUR") {
+      return "European Union";
+    } else if (curencycode === "GEL") {
+      return "Georgia";
+    } else {
+      return "Unknown currency";
+    }
+  }
+}
+console.log(getcurencycountry("USD"));
+console.log(getcurencycountry("EUR"));
+console.log(getcurencycountry("GEL"));
+console.log(getcurencycountry("JPY"));
